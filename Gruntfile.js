94b493ee871149ee051207bc46d083515d16d61b
Ondřej Linek <ondrej.linek@gmail.com>
2015-03-30 21:51:49 +0200

    Adding Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['src/**/*.js'],
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options: {
+				globals: {
+					jQuery: true
+				}
+			}
+		},
+		uglify: {
+			options: {
+				// the banner is inserted at the top of the output
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src',
+					src: '**/*.js',
+					dest: 'dist'
+				}]
+			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['jshint']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.registerTask('default', ['jshint', 'uglify']);
+};
